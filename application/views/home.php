7f5b66bd694b444e36ed73565264b56189d30353
CarolinaPerezFlores <karo0wk@gmail.com>
2020-09-21 09:31:10 -0500

    first commit

---
+++ application/views/home.php
@@ -0,0 +1,396 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE HTML>
+<!--
+	Dopetrope by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>wooposter</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="homepage is-preload">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<section id="header">
+
+					<!-- Logo -->
+						<h1><a href="index.html">wooposter</a></h1>
+
+					<!-- Nav -->
+						<nav id="nav">
+							<ul>
+								<li class="current"><a href="index.html">Inicio</a></li>
+								<li>
+									<a href="#">Servicios</a>
+									<ul>
+										<li><a href="#">Tienda online</a></li>
+										<li><a href="#">Hosting</a></li>
+										<li><a href="#">Plugins</a></li>
+										<li>
+											<a href="#">Seo</a>
+											<ul>
+												<li><a href="#">Alta en buscadores de google</a></li>
+												<li><a href="#">Alta en google negocios</a></li>
+												<li><a href="#">Google analitics</a></li>
+											</ul>
+										</li>
+										<li><a href="#">Veroeros feugiat</a></li>
+									</ul>
+								</li>
+								<li><a href="left-sidebar.html">Planes</a></li>
+								<li><a href="right-sidebar.html">Blog</a></li>
+							</ul>
+						</nav>
+
+					<!-- Banner -->
+						<section id="banner">
+							<header>
+								<h2>Lleva tu negocio a la web.</h2>
+								<p>sitios woocommerce</p>
+							</header>
+						</section>
+
+					<!-- Intro -->
+						<section id="intro" class="container">
+							<div class="row">
+								<div class="col-4 col-12-medium">
+									<section class="first">
+										<i class="icon solid featured fa-cog"></i>
+										<header>
+											<h2>Autoadministrable</h2>
+										</header>
+										<p>Administra tu inventario de productos, fotos y descripciones.</p>
+									</section>
+								</div>
+								<div class="col-4 col-12-medium">
+									<section class="middle">
+										<i class="icon solid featured alt fa-bolt"></i>
+										<header>
+											<h2>Creación rápida</h2>
+										</header>
+										<p>Tu página web en tan solo 2 semenas.</p>
+									</section>
+								</div>
+								<div class="col-4 col-12-medium">
+									<section class="last">
+										<i class="icon solid featured alt2 fa-star"></i>
+										<header>
+											<h2>Especialisada</h2>
+											<h2>en comercio</h2>
+										</header>
+										<p>Con la tecnología de woocommerce</p>
+									</section>
+								</div>
+							</div>
+							<footer>
+								<ul class="actions">
+									<li><a href="#" class="button large">Empezar</a></li>
+									<li><a href="#" class="button alt large">Leer más</a></li>
+								</ul>
+							</footer>
+						</section>
+
+				</section>
+
+			<!-- Main -->
+				<section id="main">
+					<div class="container">
+						<div class="row">
+							<div class="col-12">
+
+								<!-- Portfolio -->
+									<section>
+										<header class="major">
+											<h2>Elige un plan</h2>
+										</header>
+										<div class="row">
+											<div class="col-4 col-6-medium col-12-small">
+												<section class="box">
+													<a href="#" class="image featured"><img id="size-img" src="images/graficoc.png" alt="" /></a>
+													<header>
+														<h3>Tienda online 1</h3>
+													</header>
+													<ul>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Tecnología woocommerce</li>
+														<li><img class="check-img"src="images/comprobar.png" alt="comprobar">Hosting</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Dominio</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Email</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Alta en buscador de google</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Google Analitics</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Capacitación</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">SSL</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Elección entre cientos de temas</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Logo sensillo(2 opciones)</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Copias de seguridad semanales</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Capacitación</li>
+													</ul>
+													<!-- <p>Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.</p> -->
+													<footer>
+														<ul class="div-button-action">
+															<li><a href="#" class="button alt">Saber más</a></li>
+														</ul>
+													</footer>
+												</section>
+											</div>
+											<div class="col-4 col-6-medium col-12-small">
+												<section class="box">
+													<a href="#" class="image featured"><img id="size-img" src="images/carritoc.png" alt="" /></a>
+													<header>
+														<h3>Tienda online 2</h3>
+													</header>
+													<ul>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Tecnología woocommerce</li>
+														<li><img class="check-img"src="images/comprobar.png" alt="comprobar">Hosting</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Dominio</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Email</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Alta en buscador de google</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Google Analitics</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Capacitación</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">SSL</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Elección entre miles de temas</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Logo sensillo(3 opciones)</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Capacitación</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Copias de seguridad semanales</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Incluye chat de facebook/whatsapp</li>
+													</ul>
+													<footer>
+														<ul class="div-button-action">
+															<li><a href="#" class="button alt">Saber más</a></li>
+														</ul>
+													</footer>
+												</section>
+											</div>
+											<div class="col-4 col-6-medium col-12-small">
+												<section class="box">
+													<a href="#" class="image featured"><img id="size-img" src="images/grafico.png" alt="" /></a>
+													<header>
+														<h3>Tienda online 3</h3>
+													</header>
+													<ul>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Tecnología woocommerce</li>
+														<li><img class="check-img"src="images/comprobar.png" alt="comprobar">Hosting</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Dominio</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Email</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Alta en buscador de google</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Google Analitics</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Capacitación</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">SSL</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Elección entre cientos de temas</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Logo personalizado(2 opciones)</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Capacitación</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Copias de seguridad diarias</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Incluye chat de facebook/whatsapp</li>
+														<li><img class="check-img" src="images/comprobar.png" alt="comprobar">Creación de 2 plugins a la medida</li>
+														
+
+													</ul>
+													<footer>
+														<ul class="div-button-action">
+															<li><a href="#" class="button alt">Saber más</a></li>
+														</ul>
+													</footer>
+												</section>
+											</div>
+										</div>
+									</section>
+
+							</div>
+							<div class="col-12">
+
+								<!-- Blog -->
+									<section>
+										<header class="major">
+											<h2>Blog</h2>
+										</header>
+										<div class="row">
+											<div class="col-6 col-12-small">
+												<section class="box">
+													<a href="#" class="image featured"><img class="img-blog" src="images/image1224.png" alt="" /></a>
+													<header>
+														<h3>¿Cuánto mejoran las ventas con una página web?</h3>
+														<p>Posteado hace 45 minutos</p>
+													</header>
+													<p>Se ha demostrado un incremento de un % de ventas con la utilización de sitios web.</p>
+													<footer>
+														<ul class="actions">
+															<li><a href="#" class="button icon solid fa-file-alt">Leer</a></li>
+															<li><a href="#" class="button alt icon solid fa-comment">33 comentarios</a></li>
+														</ul>
+													</footer>
+												</section>
+											</div>
+											<div class="col-6 col-12-small">
+												<section class="box">
+													<a href="#" class="image featured"><img class="img-blog" src="images/comm.jpg" alt="" /></a>
+													<header>
+														<h3>Aptent veroeros aliquam</h3>
+														<p>Posted 45 minutes ago</p>
+													</header>
+													<p>Lorem ipsum dolor sit amet sit veroeros sed et blandit consequat sed veroeros lorem et blandit adipiscing feugiat phasellus tempus hendrerit, tortor vitae mattis tempor, sapien sem feugiat sapien, id suscipit magna felis nec elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos lorem ipsum dolor sit amet.</p>
+													<footer>
+														<ul class="actions">
+															<li><a href="#" class="button icon solid fa-file-alt">Continue Reading</a></li>
+															<li><a href="#" class="button alt icon solid fa-comment">33 comments</a></li>
+														</ul>
+													</footer>
+												</section>
+											</div>
+										</div>
+									</section>
+
+							</div>
+						</div>
+					</div>
+				</section>
+
+			<!-- Footer -->
+				<section id="footer">
+					<div class="container">
+						<div class="row">
+							<div class="col-8 col-12-medium">
+								<!-- <section>
+									<header>
+										<h2>Blandit nisl adipiscing</h2>
+									</header>
+									<ul class="dates">
+										<li>
+											<span class="date">Jan <strong>27</strong></span>
+											<h3><a href="#">Lorem dolor sit amet veroeros</a></h3>
+											<p>Ipsum dolor sit amet veroeros consequat blandit ipsum phasellus lorem consequat etiam.</p>
+										</li>
+										<li>
+											<span class="date">Jan <strong>23</strong></span>
+											<h3><a href="#">Ipsum sed blandit nisl consequat</a></h3>
+											<p>Blandit phasellus lorem ipsum dolor tempor sapien tortor hendrerit adipiscing feugiat lorem.</p>
+										</li>
+										<li>
+											<span class="date">Jan <strong>15</strong></span>
+											<h3><a href="#">Magna tempus lorem feugiat</a></h3>
+											<p>Dolore consequat sed phasellus lorem sed etiam nullam dolor etiam sed amet sit consequat.</p>
+										</li>
+										<li>
+											<span class="date">Jan <strong>12</strong></span>
+											<h3><a href="#">Dolore tempus ipsum feugiat nulla</a></h3>
+											<p>Feugiat lorem dolor sed nullam tempus lorem ipsum dolor sit amet nullam consequat.</p>
+										</li>
+										<li>
+											<span class="date">Jan <strong>10</strong></span>
+											<h3><a href="#">Blandit tempus aliquam?</a></h3>
+											<p>Feugiat sed tempus blandit tempus adipiscing nisl lorem ipsum dolor sit amet dolore.</p>
+										</li>
+									</ul>
+								</section> -->
+							</div>
+							<div class="col-4 col-12-medium">
+								<section>
+									<header>
+										<h2>What's this all about?</h2>
+									</header>
+									<a href="#" class="image featured"><img src="images/pic10.jpg" alt="" /></a>
+									<p>
+										Este es <strong>wooposter</strong> creación de woocommerce
+										<a href="http://twitter.com/ajlkn">AJ</a> for <a href="http://html5up.net/">HTML5 UP</a> It's released for free under
+										the <a href="http://html5up.net/license/">Creative Commons Attribution</a> license so feel free to use it for any personal or commercial project &ndash; just don't forget to credit us!
+									</p>
+									<footer>
+										<ul class="actions">
+											<li><a href="#" class="button">Find out more</a></li>
+										</ul>
+									</footer>
+								</section>
+							</div>
+							<div class="col-4 col-6-medium col-12-small">
+								<section>
+									<header>
+										<h2>Tempus consequat</h2>
+									</header>
+									<ul class="divided">
+										<li><a href="#">Lorem ipsum dolor sit amet sit veroeros</a></li>
+										<li><a href="#">Sed et blandit consequat sed tlorem blandit</a></li>
+										<li><a href="#">Adipiscing feugiat phasellus sed tempus</a></li>
+										<li><a href="#">Hendrerit tortor vitae mattis tempor sapien</a></li>
+										<li><a href="#">Sem feugiat sapien id suscipit magna felis nec</a></li>
+										<li><a href="#">Elit class aptent taciti sociosqu ad litora</a></li>
+									</ul>
+								</section>
+							</div>
+							<div class="col-4 col-6-medium col-12-small">
+								<section>
+									<header>
+										<h2>Ipsum et phasellus</h2>
+									</header>
+									<ul class="divided">
+										<li><a href="#">Lorem ipsum dolor sit amet sit veroeros</a></li>
+										<li><a href="#">Sed et blandit consequat sed tlorem blandit</a></li>
+										<li><a href="#">Adipiscing feugiat phasellus sed tempus</a></li>
+										<li><a href="#">Hendrerit tortor vitae mattis tempor sapien</a></li>
+										<li><a href="#">Sem feugiat sapien id suscipit magna felis nec</a></li>
+										<li><a href="#">Elit class aptent taciti sociosqu ad litora</a></li>
+									</ul>
+								</section>
+							</div>
+							<div class="col-4 col-12-medium">
+								<section>
+									<header>
+										<h2>Vitae tempor lorem</h2>
+									</header>
+									<ul class="social">
+										<li><a class="icon brands fa-facebook-f" href="#"><span class="label">Facebook</span></a></li>
+										<li><a class="icon brands fa-twitter" href="#"><span class="label">Twitter</span></a></li>
+										<li><a class="icon brands fa-dribbble" href="#"><span class="label">Dribbble</span></a></li>
+										<li><a class="icon brands fa-tumblr" href="#"><span class="label">Tumblr</span></a></li>
+										<li><a class="icon brands fa-linkedin-in" href="#"><span class="label">LinkedIn</span></a></li>
+									</ul>
+									<ul class="contact">
+										<li>
+											<h3>Address</h3>
+											<p>
+												Untitled Incorporated<br />
+												1234 Somewhere Road Suite<br />
+												Nashville, TN 00000-0000
+											</p>
+										</li>
+										<li>
+											<h3>Mail</h3>
+											<p><a href="#">someone@untitled.tld</a></p>
+										</li>
+										<li>
+											<h3>Phone</h3>
+											<p>(800) 000-0000</p>
+										</li>
+									</ul>
+								</section>
+							</div>
+							<div class="col-12">
+
+								<!-- Copyright -->
+									<div id="copyright">
+										<ul class="links">
+											<li>&copy; wooposter</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+										</ul>
+									</div>
+
+							</div>
+						</div>
+					</div>
+				</section>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
